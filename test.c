0745176ec8ae0a4c64e586e28361bb5098370033
AlanJoji <alanjojiveliyath@yahoo.com>
2022-06-07 22:57:24 +0530

    File was created; intial contents are added.

---
+++ test.c
@@ -0,0 +1,50 @@
+/* 
+Address book 
+Created by Alan Joji, Aniket Choudhary, Akshat Gouniyal
+Date : 07/06/2022
+*/
+
+// Header files
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure needs to be implemented
+
+
+// Function definitions
+/*
+1. Adding contact
+2. View contacts
+3. Edit contacts
+4. Delete contacts
+5. Exit
+*/
+
+int exit_clicked ()
+{
+    exit(0);
+
+    return 0;
+}
+
+// Main Function
+int main ()
+{
+    printf("Welcome to the Address Book");
+
+    printf("\n\n+============+============+");
+
+    printf("\n\n1. Add a contact");
+    printf("\n2. View saved contacts");
+    printf("\n3. Edit saved contacts");
+    printf("\n4. Delete a saved contact");
+    printf("\n5. Export saved contacts"); // Functionality will be implemented later
+
+    printf("\n6. Exit");
+
+    printf("\n\n+============+============+");
+
+    printf("\n\nEnter a choice : ");
+
+    return 0;
+}
