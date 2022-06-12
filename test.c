39a0ccbc77fcd36fe3f9d3972c5c10f8f65054b5
AlanJoji <alanjojiveliyath@yahoo.com>
2022-06-12 17:20:50 +0530

    Status Update

--- test.c
+++ test.c
@@ -8,20 +8,82 @@ Date : 07/06/2022
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NAME_LIMIT 20
+#define LOC_LIMIT 50
+
 // Structure needs to be implemented
+struct address
+{
+    long int phone;
+    char name[NAME_LIMIT];
+    char location[LOC_LIMIT];
+};
+
+int num;
+struct address ad[100];  
 
 
 // Function definitions
-/*
-1. Adding contact
-2. View contacts
-3. Edit contacts
-4. Delete contacts
-5. Exit
-*/
-
-int exit_clicked ()
+int add_select ()
 {
+    printf("Add a contact"); 
+
+    printf("\nEnter a name : ");
+    fgets(ad[num].name, NAME_LIMIT, stdin);
+
+
+    printf("\nEnter a phone number : ");
+    scanf("%ld", &ad[num].phone);
+
+    printf("\nEnter an address : ");
+    fgets(ad[num].location, LOC_LIMIT, stdin);
+
+    printf("Address has been saved");
+
+    for(int j=0;j<1;j++)
+    {    
+        printf("Name:%s", ad[j].name);    
+    }
+    num++;
+
+    return 0;
+}
+
+int view_select ()
+{
+    printf("View saved contacts");    
+
+
+    return 0;
+}
+
+int edit_select ()
+{
+    printf("Edit a contact");    
+
+    
+    return 0;
+}
+
+int delete_select ()
+{
+    printf("Delete a contact");    
+
+    
+    return 0;
+}
+
+int export_select ()
+{
+    printf("Export contacts to a file");    
+  
+    return 0;
+}
+
+
+int exit_select ()
+{
+    printf("Thank you!");
     exit(0);
 
     return 0;
@@ -45,6 +107,40 @@ int main ()
     printf("\n\n+============+============+");
 
     printf("\n\nEnter a choice : ");
+
+    int choice;
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1 :
+        add_select();
+        break;
+    
+    case 2 :
+        view_select();
+        break;
+    
+    case 3 :
+        edit_select();
+        break;
+    
+    case 4 :
+        delete_select();
+        break;
+
+    case 5 : 
+        export_select();
+        break;
+
+    case 6 : 
+        exit_select();
+        break;
+
+    default:
+        printf("Invalid Option");
+        break;
+    }
 
     return 0;
 }
